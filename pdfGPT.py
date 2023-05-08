9efe0a5f87ad3cf98183283700c97407004ae23b
dennisshum <108983859+dennisshum@users.noreply.github.com>
2023-05-08 08:52:13 +0000

    Add memory

--- pdfGPT.py
+++ pdfGPT.py
@@ -6,6 +6,8 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
 
 os.getenv("OPENAI_API_KEY")
 
@@ -16,7 +18,9 @@ llm = OpenAI(temperature=0.9)
 st.title("pdfGPT")
 
 # upload file
-pdfs = st.file_uploader("Upload your PDF", type="pdf", accept_multiple_files=True)
+pdfs = st.file_uploader("Upload your PDF",
+                        type="pdf",
+                        accept_multiple_files=True)
 
 # extract the text
 if pdfs:
@@ -27,23 +31,37 @@ if pdfs:
       text += page.extract_text()
 
   # split into chunks
-  text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size=1000,
-    chunk_overlap=200,
-    length_function=len
-  )
+  text_splitter = CharacterTextSplitter(separator="\n",
+                                        chunk_size=1000,
+                                        chunk_overlap=200,
+                                        length_function=len)
   chunks = text_splitter.split_text(text)
 
   # create embeddings
   embeddings = OpenAIEmbeddings()
   knowledge_base = FAISS.from_texts(chunks, embeddings)
-  
+
+  # memory
+  memory = ConversationBufferMemory(memory_key="chat_history")
+
+  chain = ConversationalRetrievalChain.from_llm(llm,
+                                                knowledge_base.as_retriever(),
+                                                memory=memory)
+
+  def answer(user_question):
+    #docs = knowledge_base.similarity_search(user_question)
+    #chain = load_qa_chain(llm, chain_type="stuff")
+    #response = chain.run(input_documents=docs, question=user_question)
+    #st.write(response)
+    response = chain({"question": user_question})
+    st.write(response["answer"])
+
   # show user input
   user_question = st.text_input("Question:")
   if user_question:
-    docs = knowledge_base.similarity_search(user_question)
-    chain = load_qa_chain(llm, chain_type="stuff")
-    response = chain.run(input_documents=docs, question=user_question)
-    st.write(response)
-  
+    answer(user_question)
+
+    with st.expander('Chat History'):
+      st.info(chain.memory.buffer)
+  #else:
+  #answer("summarize in 50 words")
